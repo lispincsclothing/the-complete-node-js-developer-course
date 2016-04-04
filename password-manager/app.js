ac074c5297417eaaa31b6ec8d35d45df62c277e3
lispincsclothing <scullingnull+1@gmail.com>
2016-04-04 16:53:24 -0700

    Initial working persistent storage - prints object object rather than contents of object

--- password-manager/app.js
+++ password-manager/app.js
@@ -1,1 +1,15 @@
+'use strict';
+
 console.log('starting password manager');
+
+var storage = require('node-persist');
+storage.initSync();
+
+// storage.setItemSync('name', 'Andrew');
+var accounts = storage.getItemSync('accounts');
+// var accounts = [];
+accounts.push({username:'cookie', balance:0})
+console.log('Saved accounts  are: ' + accounts);
+storage.setItemSync('accounts', accounts );
+
+console.log('Saved accounts  are: ' + accounts);
