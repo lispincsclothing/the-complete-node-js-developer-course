2a515288c59dd9767eef55633229fe702040e195
lispincsclothing <scullingnull+1@gmail.com>
2016-04-04 21:03:27 -0700

    Yargs exercise - not the greatest, feels like cutting and pasting

--- password-manager/example-args.js
+++ password-manager/example-args.js
@@ -1,4 +1,19 @@
-var argv = require('yargs').argv;
+var argv = require('yargs').command('hello', 'Greets the user', function(yargs) {
+    yargs.options({
+      name: {
+        demand: true,
+        alias: 'n',
+        description: 'Your first name goes here'
+      },
+      lastname: {
+        demand: true,
+        alias: 'l',
+        description: 'Your last name goes here'
+      },
+    }).help('small help');
+  })
+  .help('help')
+  .argv;
 var command = argv._[0];
 
 console.log(argv);
