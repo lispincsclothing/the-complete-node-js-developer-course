6f17167895c063fb5a2c12e0e487a3afe4d5294d
lispincsclothing <scullingnull+1@gmail.com>
2016-04-05 21:06:42 -0700

    Illustration of using modules by getting location from ipinfo.io

--- async/weather.js
+++ async/weather.js
@@ -1,5 +1,5 @@
 var request = require('request');
-var url = "http://api.openweathermap.org/data/2.5/weather?units=imperial&zip=90210,us&appid=DELETED
+var url = 'http://api.openweathermap.org/data/2.5/weather?zip=90210,us&units=imperial&appid=239fe7b91b52663184a96317f945d01e'
 
 module.exports = function (callback) {
   request({
