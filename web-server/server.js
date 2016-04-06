8289b6caf81d3b90b47bed94e094b3d2859a0323
lispincsclothing <scullingnull+1@gmail.com>
2016-04-06 13:25:16 -0700

    Serving static assets (in this case index.html)

--- web-server/server.js
+++ web-server/server.js
@@ -1,13 +1,13 @@
 var express = require('express');
 var app = express();
-
-app.get('/', function (req, res) {
-  res.send('Hello Express');
-});
-
+const PORT = 3000;
 
 app.get('/about', function (req, res) {
   res.send('about us');
 });
 
-app.listen(3000);
+app.use(express.static(__dirname + '/public'));
+
+app.listen(PORT, function () {
+  console.log('Server started at port ' + PORT + '!');
+});
